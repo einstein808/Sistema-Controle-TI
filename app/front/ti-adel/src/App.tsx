e6171b8b4be8083d4218da66c0ab947471b7d6ff
einstein808 <gabryel808einstein@gmail.com>
2023-07-10 17:46:31 -0300

    criando side bar, musando diagramj de lugar

--- app/front/ti-adel/src/App.tsx
+++ app/front/ti-adel/src/App.tsx
@@ -1,21 +1,41 @@
 
 import './App.css'
-import { FaComputer } from 'react-icons/fa'
-
+import { RiComputerFill} from "react-icons/ri";
+import { BiSolidUserCircle } from "react-icons/bi";
+import {BsKeyboardFill} from "react-icons/bs";
+import {BsFillTelephoneFill}  from "react-icons/bs";
+import {FaTowerCell} from "react-icons/fa6";
 function App() {
  
 
   return (
     <>
       <div className='flex'>
-        <div className='min-h-screen w-40 bg-gray-200 pr-2'>
+        <div className='min-h-screen w-120 bg-gray-200 pr-2'>
           <ul className='mt-5 ml-2  text-lg cursor-pointer'>
-            <li className='mb-1  hover:bg-blue-400 pl-3 rounded-lg flex hover:text-white pb-1'>
-              Computadores
+            <li className='mb-1  hover:bg-blue-400 pl-3 rounded-lg  flex hover:text-white pb-1 pr-2'>
+            <RiComputerFill className="mt-1.5 mr-1 "  />  
+            Computadores
             </li>
             <li className='mb-1  hover:bg-blue-400 pl-3 rounded-lg flex hover:text-white pb-1'>
+              <BiSolidUserCircle className="mt-1.5 mr-1 "/>
               Usuário
-              <FaComputer/>
+              
+            </li>
+            <li className='mb-1  hover:bg-blue-400 pl-3 rounded-lg flex hover:text-white pb-1'>
+              <BsKeyboardFill className="mt-1.5 mr-1 "/>
+              Periféricos
+              
+            </li>
+            <li className='mb-1  hover:bg-blue-400 pl-3 rounded-lg flex hover:text-white pb-1'>
+              <BsFillTelephoneFill className="mt-1.5 mr-1 "/>
+              Telefone
+              
+            </li>
+            <li className='mb-1  hover:bg-blue-400 pl-3 rounded-lg flex hover:text-white pb-1'>
+              <FaTowerCell className="mt-1.5 mr-1 "/>
+              Internet
+              
             </li>
           </ul>
 
