aff8aa7df9490ac60682b516da41bbd77b6c4042
einstein808 <gabryel808einstein@gmail.com>
2023-07-12 16:12:34 -0300

    criando projeto vite e elecron

--- app/front/ti-adel/.eslintrc.cjs
+++ app/front/ti-adel/.eslintrc.cjs
@@ -1,14 +1,22 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    sourceType: 'module',
+    ecmaVersion: 2021
+  },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended'
+  ]
 }
