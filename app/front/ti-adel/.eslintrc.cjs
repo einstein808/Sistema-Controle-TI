121e22f5a2a903fdbd473a231a9bf8a1961039ee
einstein808 <gabryel808einstein@gmail.com>
2023-07-13 17:50:29 -0300

    mudando escopo do projeto colocando vite e electron

--- app/front/ti-adel/.eslintrc.cjs
+++ app/front/ti-adel/.eslintrc.cjs
@@ -1,22 +1,14 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    node: true
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    sourceType: 'module',
-    ecmaVersion: 2021
-  },
+  env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:prettier/recommended'
-  ]
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+  },
 }
